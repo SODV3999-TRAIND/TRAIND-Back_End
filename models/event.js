622c2b7826334263728b27c7cba0bf6073407018
mapersson <62900241+mapersson@users.noreply.github.com>
2021-02-09 07:54:54 -0700

    Rough Events
    Added model information and rough routing for working with events.

--- models/event.js
+++ models/event.js
@@ -8,6 +8,20 @@ const eventSchema = new mongoose.Schema({
   location: { latitude: Number, longitude: Number },
 });
 
+/*
+eventSchema.method("validateTime", function () {
+  const trainer = this.organizer;
+  const attendee = this.attendee;
+  event.findById(trainer, "startDate", (err, date) => {
+    if (err) {
+      console.log("Event Error");
+    } else {
+      console.log(date.toString());
+    }
+  });
+});
+*/
+
 const event = mongoose.model("event", eventSchema);
 
 module.exports = event;
