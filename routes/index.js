b41dba45067fa65a54ed6c169d9786711057b8ae
mapersson <62900241+mapersson@users.noreply.github.com>
2021-02-09 18:02:54 -0700

    Remove Old Trainer Code
    Removed all trainer code as its needs to be rebuilt for MongoDB client.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const trainRoute = require("./trainer");
 const clientRoute = require("./client");
 const eventRoute = require("./event");
 
-// router.get("/hello", (req, res) => {
-//   res.send(`Hello from Router ${Test}`);
-// });
-
-// router.use("/trainer", trainRoute);
 router.use("/client", clientRoute);
 router.use("/event", eventRoute);
 
