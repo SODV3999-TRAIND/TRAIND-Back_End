30283a9b4f849627b6fddc7a3bf7e1a0139818d1
Avneet Sidhu <a.sidhu499@mybvc.ca>
2021-02-18 15:00:42 +0530

    added post and get method for email and password

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,34 @@
 const express = require("express");
 const router = express.Router();
+
+var users =[ {
+    email: 'abc@gmail.com' ,password: 'password'
+}
+]
+
+router.get('/',function(req, res, next){
+    res.send("Welcome to backend server");
+
+});
+
+router.post('/formm', function(req, res){
+ let result = users.find(user => user.email == req.body.email);
+ if(result) {
+     if(result.password == req.body.password){
+         res.status(200).send( {
+             message: "Successful login!!"
+         })
+     }else {
+         res.status(200).send( {
+             message: "Password incorrect!!"
+         })
+     }
+ }else {
+     res.status(200).send( {
+         message: "User not found"
+     })
+ }
+})
 const clientRoute = require("./client");
 const eventRoute = require("./event");
 
